a34422eefc6eb127f775d46feb4ca4929ce4c62a
Gilson <gilsonalves@aedu.com>
2020-08-07 16:28:46 -0300

    Atualizacao do FrameWork Laravel de 5.5 para 5.7

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,9 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> {{$titulo}} </title>
-    <link href="../../../css/app.css" rel="stylesheet">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
+<div class="container">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <ul class="navbar ml-auto" >
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Sair</li>
+        </ul>
+    </nav>
+</div>
 <div class="container">
     <div class="jumbotron">
         <h1>@yield("cabecalho")</h1>
@@ -17,5 +26,6 @@
     @yield("conteudo")
 
 </div>
+<script src="{{ asset('js/app/js') }}"></script>
 </body>
 </html>
